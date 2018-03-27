56063f24566d2b9bea9e78f7514c1a350c0070f4
mack-wang <641212003@qq.com>
2018-03-27 17:16:23 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const Random = Mock.Random;
 
 const router = express.Router();
-const upload = multer({ dest: 'upload_tmp/' });
+const upload = multer({ dest: './upload/' });
 /*
 *
 GET /zoos：列出所有动物园
@@ -433,7 +433,7 @@ router.post('/upload', upload.any(), (req, res, next) => {
           filename: req.files[0].originalname,
         };
         console.log(response);
-        res.end(JSON.stringify(response));
+        res.json(response);
       }
     });
   });
